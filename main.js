69ab9cb206f3af365955b08aa0700caf9f67fabe
iDominate <68128371+iDominate@users.noreply.github.com>
2023-03-03 07:14:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,107 @@
+var mon = document.getElementById('mon');
+var tue = document.getElementById('tue');
+var wed = document.getElementById('wed');
+var thu = document.getElementById('thu');
+var fri = document.getElementById('fri');
+var sat = document.getElementById('sat');
+var sun = document.getElementById('sun');
+
+mon.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+mon.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+tue.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+tue.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+wed.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+wed.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+thu.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+thu.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+fri.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+fri.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+sat.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+sat.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+sun.onmousemove = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'visible';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+}
+
+sun.onmouseleave = function () {
+    var popup = this.getElementsByTagName('p')[0];
+    popup.style.visibility = 'hidden';
+    var bar = this.getElementsByClassName('bar-chart')[0];
+    bar.style.backgroundColor = 'hsl(10, 79%, 65%)';
+}
+
+
